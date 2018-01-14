ae4c1c2f77cf05e9e351211026fa6f385542db89
paulkleinschmidt <paul.kleinschmidt1@gmail.com>
2018-01-14 11:10:50 -0500

    Adds header opacity on scroll
    header becomes more transparent on scroll
    on mouse hover the header goes back to full opacity

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 $(() => {
-
+  const shrinkHeader = 300
+  $(window).scroll(function () {
+    const scroll = getCurrentScroll()
+    if (scroll >= shrinkHeader) {
+      $('.header').addClass('shrink')
+    } else {
+      $('.header').removeClass('shrink')
+    }
+  })
+  function getCurrentScroll () {
+    return window.pageYOffset || document.documentElement.scrollTop
+  }
 })
